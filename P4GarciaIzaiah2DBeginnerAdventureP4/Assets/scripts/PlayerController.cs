98e3473ed4e1c4da153da390f5f721cf1fcc24b5
749759-Cloud <749759@student.dusd.net>
2025-11-21 11:09:58 -0800

    Fixed repository
    Fix the repository from files.

---
+++ P4GarciaIzaiah2DBeginnerAdventureP4/Assets/scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        
+}
